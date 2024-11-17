c7c51aec8d2fb6717fadddf8bd1c0c32024ddf78
Calistacwt <seastheskies.cwt@gmail.com>
2024-11-18 03:16:11 +0800

    fix: Refractor code for Register SR form

--- src/modules/registerSR/component/infoCard.tsx
+++ src/modules/registerSR/component/infoCard.tsx
@@ -1,50 +1,81 @@
 const InfoCard = () => {
   return (
-    <div className="border bg-white p-2 lg:p-10  border-red-600">
-      <div className="bg-gray-100 p-2 border border-green-600">
-        <div className="flex justify-between mb-3">
-          <div className="box-content h-30 w-40 p-2">
-            <img
-              src="./assets/images/KKHLogoPNG.svg"
-              className="object-fit-contain"
-            ></img>
-          </div>
-          <div className="p-2 content-center">
-            <h3 className="font-bold text-md mb-3 2">
-              Senior Resident Details
-            </h3>
-          </div>
-          {/* <div className="p-2">Flex item</div> */}
+    <div className="bg-background rounded-lg mt-10 p-6">
+      <div className="flex justify-between items-center space-x-5">
+        <div>
+          <img
+            src="./assets/images/KKHLogo_transparent.svg"
+            alt="KKH logo"
+            className="w-36"
+          />
         </div>
 
-        <hr className="border border-updateSR-hr" />
+        <div>
+          <h3 className="font-semibold text-sm">Senior Resident Details</h3>
+        </div>
+      </div>
 
-        <div className="mt-3 text-sm font-medium">
-          <p>Posting Period</p>
-          <p>-</p>
+      <hr className="border border-sidebar-border" />
 
-          <div className="flex justify-between mb-3 ">
-            <p className=" flex-1 text-start">Senior Resident</p>
-            <p className=" flex-1 text-start content-center">MCR</p>
-            <p className="flex-1 text-start content-center">Mobile</p>
-            <p className="flex-1 text-start content-center">Email</p>
+      <div className="flex flex-col space-y-6 p-4">
+        <div className="flex-1 space-y-2">
+          <label className="text-card-text font-semibold text-xs">
+            Posting Period
+          </label>
+          <p className="font-semibold text-xs">06 May 2024 - 02 June 2024</p>
+        </div>
+
+        <div className="flex justify-between flex-col xl:flex-row space-y-5 xl:space-y-0  ">
+          <div className="space-y-2 ">
+            <label className="text-card-text font-semibold text-xs">
+              Senior Resident
+            </label>
+            <p className="font-semibold text-xs">Senior 1</p>
+          </div>
+          <div className="space-y-2">
+            <label className="text-card-text font-semibold text-xs">MCR</label>
+            <p className="font-semibold text-xs">M12345E</p>
           </div>
 
-          <div className="flex justify-between mb-3">
-            <p className="flex-1 text-start">-</p>
-            <p className="flex-1 text-start">-</p>
-            <p className="flex-1 text-start">-</p>
-            <p className="flex-1 text-start">-</p>
+          <div className="space-y-2">
+            <label className="text-card-text font-semibold text-xs">
+              Mobile
+            </label>
+            <p className="font-semibold text-xs">+6598764321</p>
           </div>
 
-          <p>Call Dates</p>
-          <p>-</p>
-          <p>Leave</p>
-          <p>-</p>
-          <p>No of DCD sessions after deducting leave and workshop</p>
-          <p>-</p>
-          <p>Remarks</p>
-          <p>-</p>
+          <div className="space-y-2">
+            <label className="text-card-text font-semibold text-xs">
+              Email
+            </label>
+            <p className="font-semibold text-xs">senior1@mohh.com.sg</p>
+          </div>
+        </div>
+
+        <div className="flex-1 space-y-2">
+          <label className="text-card-text font-semibold text-xs">
+            Call Dates
+          </label>
+          <p className="font-semibold text-xs">06 May 2024 - 02 June 2024</p>
+        </div>
+
+        <div className="flex-1 space-y-2">
+          <label className="text-card-text font-semibold text-xs">Leaves</label>
+          <p className="font-semibold text-xs">06 May 2024 - 02 June 2024</p>
+        </div>
+
+        <div className="flex-1 space-y-2">
+          <label className="text-card-text font-semibold text-xs">
+            No of DCD sessions after deducting leave and workshop
+          </label>
+          <p className="font-semibold text-xs">30 Session</p>
+        </div>
+
+        <div className="flex-1  space-y-2">
+          <label className="text-card-text font-semibold text-xs">
+            Remarks
+          </label>
+          <p className="font-semibold text-xs">-</p>
         </div>
       </div>
     </div>
--- src/modules/registerSR/index.tsx
+++ src/modules/registerSR/index.tsx
@@ -1,21 +1,35 @@
-import RegistrationForm from "./component/registrationForm";
 import InfoCard from "./component/infoCard";
+import RegisterForm from "./component/registerForm";
 
-const ScheduleSR = () => {
+const RegisterSR = () => {
   return (
     <div>
       <div className=" mb-3 space-y-1">
         <h1 className="font-bold text-xl">Senior Resident Registration</h1>
-        <h6 className="text-xxs text-[#5C5C5C]">
+        <h6 className="text-xs text-dashboard-text">
           Entry of Senior Resident Doctor Information
         </h6>
       </div>
-      <div className="flex flex-col lg:flex-row lg:space-x-4">
-        <RegistrationForm />
-        <InfoCard />
+
+      <div className="bg-white  p-8 rounded-xl mt-5">
+        <div>
+          <h3 className=" font-bold text-md mb-3 ">
+            Details of Senior Resident Doctor
+          </h3>
+          <hr className="border-1 border-background-hr" />
+        </div>
+
+        <div className="flex space-x-14">
+          <div className="flex-1 ">
+            <RegisterForm />
+          </div>
+          <div className="flex-1">
+            <InfoCard />
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default ScheduleSR;
+export default RegisterSR;
