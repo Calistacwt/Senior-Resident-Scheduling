899dd9a553e26eb9f3aa4371cebeab7e7b7c17d6
DarryllV <dondarryll8@gmail.com>
2024-12-04 10:06:18 +0800

    created batch file to run the website
    does not open browser automatically

---
+++ Senior_Resident_Scheudling.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: Run npm start
+npm start
+
+:: Open Chrome browser with the specified URL
+:: start chrome "http://localhost:3000"
+
+:: Prevent the terminal from closing immediately
+pause
