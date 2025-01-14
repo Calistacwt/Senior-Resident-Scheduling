a9081d9a996ae69acc066ab8f7cdb7c567e4bba4
Calistacwt <seastheskies.cwt@gmail.com>
2025-01-14 14:35:07 +0800

    feat: import clinicSchedule

--- src/services/room.ts
+++ src/services/room.ts
@@ -1,11 +1,16 @@
 import { ROOM } from "@/config/endpoint";
-import { room } from "@/types/room";
+import { roomList } from "@/types/roomList";
 
 import axios from "axios";
 
 const BASE_URL = "http://localhost:4000";
 
-export const registerRoom = async (data: room): Promise<room> => {
+export const registerRoom = async (data: roomList): Promise<void> => {
   const response = await axios.post(`${BASE_URL}${ROOM}`, data);
+  return response.data;
+};
+
+export const getRoomData = async () => {
+  const response = await axios.get(`${BASE_URL}${ROOM}`);
   return response.data;
 };
